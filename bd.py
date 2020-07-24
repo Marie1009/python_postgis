e1c28649d84dfb618c217f04b932a6d9564568d5
Marie1009 <a20186335@pucp.edu.pe>
2020-07-24 08:53:09 +0200

    premiers tests perf

--- bd.py
+++ bd.py
@@ -5,7 +5,8 @@ import pstats
 import io
 import unittest
 import numpy as np
-
+import timeit
+import time
 from rasterio import Affine as A
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 from rasterio.io import MemoryFile
@@ -22,19 +23,24 @@ def simple_query():
 	#cur.execute("WITH test as (SELECT ST_Intersection(ST_Union(altifr_p1.rast), ST_Union(altifr_p2.rast)) FROM altifr_p1, altifr_p2)SELECT ST_SummaryStats(	ST_MapAlgebra(	test.st_intersection, 1,test.st_intersection, 2,'([rast2] - [rast1])') ) AS rast FROM test")
 
 	# query qui recupere les valeurs des pixel pour une paire lat/long donnee : 
-	cur.execute("SELECT ST_Value(altifr_p1.rast, ST_GeomFromText('POINT(175570.932 6796643.598)',2154)) FROM altifr_p1")
+	start = time.perf_counter()
 
+	cur.execute("SELECT ST_Value(altifr_p1.rast, ST_GeomFromText('POINT(175570.932 6796643.598)',2154)) FROM altifr_p1")
+	print(f"Executed in {time.perf_counter() - start:0.4f} seconds")
 	rows = cur.fetchall()
 
 	for row in rows:
 		print(row)
+
 
 def get_raster_srid(table):
 	conn = psycopg2.connect(database="postgis_test", user="postgres", host="localhost", password="admin")
 	cur = conn.cursor()
 	# query qui recupere les valeurs des pixel pour une paire lat/long donnee : 
 	cur.execute("SELECT ST_SRID(ST_Union("+ table +".rast)) FROM "+table)
+	
 	rows = cur.fetchall()
+	
 	return rows[0][0]
 	
 
@@ -47,10 +53,12 @@ def raster_query():
 
 	#query qui retourne une table raster
 	#avec ST_Union : retourne un raster sans tuiles 750x1000
+	start = time.perf_counter()
 	cur.execute("SELECT ST_AsGDALRaster(ST_Union(altifr_p1.rast), 'GTiff') FROM altifr_p1")
 	#sans union on recupere le raster decoupe en tuiles dans ce cas 250x250
 	#cur.execute("SELECT ST_AsGDALRaster(altifr_p1.rast, 'GTiff') FROM altifr_p1")
-
+	print(f"Executed in {time.perf_counter() - start:0.4f} seconds")
+	
 	rows = cur.fetchall()
 
 	value = 0 
@@ -67,15 +75,23 @@ def raster_query():
 				#read(1) returns numpy array contenant les valeurs de raster pour la bande 1
 
 				print(dataset.crs)
-				if dataset.crs != "ESPG:4326":
-					dst_crs = 'EPSG:4326'
-
-					#transform, width, height = calculate_default_transform(dataset.crs, dst_crs, dataset.width, dataset.height, *dataset.bounds)
-				    
+				#if dataset.crs != "ESPG:4326":
+					#dst_crs = 'EPSG:4326'
+					#transform, width, height = calculate_default_transform(dataset.crs, dst_crs, dataset.width, dataset.height, *dataset.bounds)			    
 					#kwargs = dataset.meta.copy()
 					#kwargs.update({  'crs': dst_crs, 'transform': transform, 'width': width,  'height': height  })
 					#dst = np.empty([750,1000])
-					#reproject(   source=data_array, destination=dst,  src_transform=dataset.transform, src_crs=dataset.crs, dst_transform=transform, dst_crs=dst_crs, resampling=Resampling.nearest)
+					#dst = dataset.read(1)
+					#print("ok")
+
+					#with MemoryFile() as mmfile:
+					#	reproject(   source=data_array, destination=dst,  src_transform=dataset.transform, src_crs=dataset.crs, dst_transform=transform, dst_crs=dst_crs, resampling=Resampling.nearest)
+					#	mmfile.write(dst)
+
+					#	with mmfile.open() as new_dataset:
+					#		new_array = new_dataset.read(1)
+					#		print(new_array)
+
 				print(data_array.shape)
 				#print(data_array)
 
@@ -84,6 +100,7 @@ def raster_query():
 				#print(data_array[0])
 				print(dataset.bounds)
 				
+				print(dataset.transform)
 				print(dataset.dtypes)
 				#print(dataset.xy(dataset.height // 2, dataset.width // 2))
 
@@ -92,8 +109,6 @@ def raster_query():
 				print(longi)
 				print(lati)
 
-				#longi = 175570.932
-				#lati = 6796643.598
 
 				indices = dataset.index(longi,lati)
 				x = indices[0]
@@ -120,9 +135,7 @@ def raster_point_query(longi,lati):
 
 	#sans union on recupere le raster decoupe en tuiles dans ce cas 250x250
 	#cur.execute("SELECT ST_AsGDALRaster(altifr_p1.rast, 'GTiff') FROM altifr_p1")
-
 	rows = cur.fetchall()
-
 	value = 0 
 
 	for row in rows:
@@ -149,8 +162,13 @@ def raster_point_query(longi,lati):
 def main():
 	#srid = get_raster_srid("altifr_p2")
 	#print(srid)
-	#cProfile.run('raster_query()')
-    raster_query()
+	
+	start = time.perf_counter()
+	#raster_query()
+	simple_query()
+	print(f"Executed in {time.perf_counter() - start:0.4f} seconds")
+
+    
 
 
 if __name__== "__main__" :
--- test_querys.py
+++ test_querys.py
@@ -1,19 +1,22 @@
 import unittest
 import bd 
+import csv
+
 
 class RasterTest(unittest.TestCase):
 
-	def test_values_file(self):
-		# [ [(long,lat), value], ..]
-		f = open("altifr_p1.txt","r")
-		lines =f.readlines()
-		for line in lines:
-			print(line)
-			liste=list(line.split(", "))
-			elt = bd.raster_point_query(float(liste[0]),float(liste[1]))
-		# verifie que elt = resultat attendu a 2 decimales pres
-			self.assertAlmostEqual(elt, float(liste[2]),2)
+    def test_values_file(self):
+        # [ [(long,lat), value], ..]
+        with open('altifr_p1.txt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            #line_count = 0
+            for line in csv_reader:
+                print(line)
+                # print (line)
+                elt = bd.raster_point_query(float(line[0]),float(line[1]))
+                # verifie que elt = resultat attendu a 2 decimales pres
+                self.assertAlmostEqual(elt, float(line[2]),2)
 
 
 if __name__== "__main__" :
-	unittest.main()
+    unittest.main()
