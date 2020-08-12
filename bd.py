06643e3c90dfcb76fd861272bf2c24d206686b1e
Marie1009 <a20186335@pucp.edu.pe>
2020-08-12 11:51:56 +0200

    pgsql function test

--- bd.py
+++ bd.py
@@ -1,18 +1,14 @@
 import psycopg2
 import rasterio
-import cProfile
-import pstats
-import io
 import unittest
 import numpy as np
 import timeit
 import time
+import random
 import csv
 
 from psycopg2 import OperationalError
 from psycopg2 import sql
-from rasterio import Affine as A
-from rasterio.warp import calculate_default_transform, reproject, Resampling
 from rasterio.io import MemoryFile
 
 
@@ -38,24 +34,20 @@ def execute_read_query(connection, query):
 		start = time.perf_counter()
 		cursor.execute(query)
 		end = time.perf_counter()
-		runtime = end - start
+		runtime_exe = end - start
 		#f.write("{} executed in {} seconds \n".format(key,runtime))
 
 		result = cursor.fetchall()
-		return [result,runtime]
+		end_fetch = time.perf_counter()
+		runtime_fetchall = end_fetch - end
+		return [result,runtime_exe, runtime_fetchall]
 	except OperationalError as e:
 		print(f"The error '{e}' occurred")
 
 
 def execute_read_query_param(connection,champ, table):
-	
-
 	query = sql.SQL("select ST_AsGDALRaster(ST_Union({field}), 'GTiff') from {table}").format(field=sql.Identifier(champ),table=sql.Identifier(table))
-    
-
-
     #pkey=sql.Identifier('id'))
-
 	#query = sql.SQL("select {field} from {table} where {pkey} = %s").format(
     #field=sql.Identifier('my_name'),
     #table=sql.Identifier('some_table'),
@@ -83,13 +75,11 @@ def get_raster_srid(table):
 	cur = conn.cursor()
 	# query qui recupere les valeurs des pixel pour une paire lat/long donnee : 
 	cur.execute("SELECT ST_SRID(ST_Union("+ table +".rast)) FROM "+table)
-	
 	rows = cur.fetchall()
-	
 	return rows[0][0]
 	
 
-def infos_raster(dataset):
+def infos_raster(dataset,data_array):
 	return [dataset.crs, data_array.shape,dataset.bounds, dataset.transform, dataset.dtypes]
 	
 
@@ -127,11 +117,11 @@ def raster_point_query(longi,lati):
 				
 				value = data_array[int(x)][int(y)]
 				print( value )
-
 	return value 
 
-def get_queries():
-	with open('queries.txt') as csv_file:
+
+def get_queries(file):
+	with open(file) as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter='#')
 		qdict = {}
 		#liste = []
@@ -141,14 +131,29 @@ def get_queries():
 
 	return qdict
 
+def get_image(connection):
+	query = "SELECT ST_AsGDALRaster(fct_get_image(ST_GeomFromText('POINT(154938.251 6821208.497)'), 500, 'altifr_75m_0150_6825'), 'GTiff')"
+	results = execute_read_query(connection,query)
+	for row in results[0]:
+		print(row)
+		rast = bytes(row[0])
+
+		with MemoryFile(rast) as memfile:
+			with memfile.open() as dataset:
+
+				data_array = dataset.read(1)
+				#read(1) returns numpy array contenant les valeurs de raster pour la bande 1
+							
+				print(infos_raster(dataset,data_array))
+
+
 def start_queries(mode,connection,queries_dict):
-	keys = queries_dict.keys()
+	keys = list(queries_dict.keys())
 	print(keys)
 	keys_list = []
 	#mode "normal" pas de changement on fait les requetes a la suite comme elles sont dans le dict
 	if mode == 0:
 		keys_list = keys
-
 	#mode double : on fait chaque requete 2 fois a la suite
 	elif mode == 1:
 		for e in keys:
@@ -158,22 +163,21 @@ def start_queries(mode,connection,queries_dict):
 	#mode aleatoire : on melange les requetes aleatoirement
 	elif mode == 2:
 		keys_list = random.sample(keys,len(keys))
-	
 
+	elif mode == 3:
+		keys.reverse()
+		keys_list = keys
+		
 	f = open("results_{}.txt".format(mode), "w")
 	for key in keys_list:
 		print(key)
 		results = execute_read_query(connection,queries_dict[key])
 		values = results[0]
-		runtime = results[1]
-		f.write("{} executed in {} seconds \n".format(key,runtime))
+		runtime_exe = results[1]
+		runtime_fetchall = results[2]
+		ratio = (runtime_fetchall / runtime_exe + runtime_fetchall)*100 
+		f.write("{} executed in {} seconds and fetched in {} seconds \n ratio : {} % \n".format(key,runtime_exe, runtime_fetchall,ratio))
 	f.close()
-
-	
-
-
-
-
 
 
 def main():
@@ -182,11 +186,15 @@ def main():
 
 	#START CONNECTION
 	connection = create_connection("postgis_test","postgres","admin","localhost","5432")
-	queries_dict = get_queries()
+	queries_dict = get_queries('queries.txt')
 	
 	
-	start_queries(0,connection,queries_dict)
+	start_queries(3,connection,queries_dict)
 	
+	#get_image(connection)
+
+
+
 	#values = test_prepared(connection)
 
 	#end = time.perf_counter()
